2db023596173843e98c851fc1041995877a18082
Trevor Vannoy <trevorvannoy@montana.edu>
2019-01-04 14:43:17 -0700

    refactored numpy indexing from arrays to slices to (drastically) improve performance

--- utils.py
+++ utils.py
@@ -31,6 +31,7 @@ def init_transform(dim, m=None):
     return V
 
 # calculate scatter matrix as the inner product of the centered data matrix with itself
+centered_data_np = np.array([])
 def calculate_scatter(data, num_processes=mp.cpu_count()):
 
     # Compute the mean vector
@@ -41,17 +42,38 @@ def calculate_scatter(data, num_processes=mp.cpu_count()):
 
     # created shared memory RawArrays for the data matrices
     shape = data.shape
-    centered_data = mp.RawArray(ctypes.c_double, data.size)
+    # centered_data = mp.RawArray(ctypes.c_double, data.size)
     scatter = mp.RawArray(ctypes.c_double, shape[1]*shape[1])
 
+    # scatter[0] = 1.5
+    # print(scatter[0])
     # wrap the centered data matrix shared memory in a numpy array
-    centered_data_np = np.frombuffer(centered_data, dtype=np.float64).reshape(shape)
+    # centered_data_np = np.frombuffer(centered_data, dtype=np.float64).reshape(shape)
 
     # copy data to the shared memory array
-    np.copyto(centered_data_np, data - mean)
+    # t0 = perf_counter()
+    # np.copyto(centered_data_np, data - mean)
+    # t1 = perf_counter()
+    # print('copyto overhead: {}'.format(t1-t0))
+    # print((centered_data_np == (data - mean)).all())
+    # print(centered_data[0])
+
+    global centered_data_np
+    centered_data_np = data - mean
+    centered_data = 0
+
+    # t0 = perf_counter()
+    # centered_data_np.T @ centered_data_np
+    # t1 = perf_counter()
+    # print('without indexing: {}'.format(t1-t0))
 
     # split the inner product up among the processes in the pool
     columns = np.array_split(np.arange(shape[1]), num_processes)
+    # t0 = perf_counter()
+    # centered_data_np.T @ centered_data_np[:,columns[0][0]:columns[0][-1]]
+    # t1 = perf_counter()
+    # print('with indexing: {}'.format(t1-t0))
+
     with mp.Pool(processes=num_processes, initializer=_init_calculate_scatter_worker, initargs=(centered_data, scatter, shape)) as pool:
         pool.map(_calculate_scatter_worker, columns)
 
@@ -67,11 +89,22 @@ def _init_calculate_scatter_worker(centered_data, scatter, shape):
 # compute part of the scatter matrix
 def _calculate_scatter_worker(columns):
     # wrap the shared memory in numpy arrays
-    centered_data_np = np.frombuffer(init['centered_data'], dtype=np.float64).reshape(init['shape'])
+    t0 = perf_counter()
+    # centered_data_np = np.frombuffer(init['centered_data'], dtype=np.float64).reshape(init['shape'])
     scatter_np = np.frombuffer(init['scatter'], dtype=np.float64).reshape(init['shape'][1], init['shape'][1])
+    t1 = perf_counter()
+    print('frombuffer overhead: {}'.format(t1-t0))
+
+    # print(centered_data_np)
+    # print(scatter_np)
 
     # compute the partial inner product of the full data matrix with the sub-matrix formed by columns
-    scatter_np[:,columns] = centered_data_np.T @ centered_data_np[:,columns]
+    t0 = perf_counter()
+    scatter_np[:,columns[0]:columns[-1]+1] = centered_data_np.T @ centered_data_np[:,columns[0]:columns[-1]+1]
+    t1 = perf_counter()
+    print('computation time: {}'.format(t1-t0))
+    # print(scatter_np)
+    # print(init['scatter'][0])
 
 
 def sorted_eig(s, m=None):
